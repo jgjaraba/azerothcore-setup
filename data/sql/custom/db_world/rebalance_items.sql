fa40f48137b7134b0059a7328924a76857f31fe4
JoseGJ <josegjaraba@gmail.com>
2026-09-25 16:54:15 +0200

    Included dbc files

--- data/sql/custom/db_world/rebalance_items.sql
+++ data/sql/custom/db_world/rebalance_items.sql
@@ -1,3 +1,4 @@
 UPDATE `item_template` SET `stat_type3` = 3, `stat_value3` = 11 WHERE (`entry` = 6975);
 UPDATE `item_template` SET `stat_type3` = 3, `stat_value3` = 11 WHERE (`entry` = 6976);
 UPDATE `item_template` SET `stat_type3` = 3, `stat_value3` = 11 WHERE (`entry` = 6977);
+UPDATE `item_template` SET `Quality` = 3, `ItemLevel` = 48, `stat_value1` = 8, `stat_value3` = 5 WHERE (`entry` = 4430);
